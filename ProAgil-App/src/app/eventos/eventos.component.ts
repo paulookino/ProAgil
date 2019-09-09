24ee6ef5700ab67f900397a66b2df3bf5eecd5bb
Okino Paulo <p.okino@reply.com>
2019-09-09 16:44:43 -0300

    37. Two-way Data Binding

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -2,18 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { error } from 'util';
 
-
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
   styleUrls: ['./eventos.component.css']
 })
+
 export class EventosComponent implements OnInit {
 
   eventos: any = [];
   imagemLargura = 50;
   imagemMargem = 2;
   mostrarImagem = false;
+  filtroLista = '';
 
   constructor(private http: HttpClient) { }
 
