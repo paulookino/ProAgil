5f33b07af8c87d7113caba5827f9ba57f6ec7900
Okino Paulo <p.okino@reply.com>
2019-10-08 10:40:44 -0300

    61. Modal

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { EventoService } from '../_services/evento.service';
 import { Evento } from '../_models/Evento';
+import {  BsModalService, BsModalRef } from 'ngx-bootstrap';
 
 
 @Component({
@@ -10,8 +11,21 @@ import { Evento } from '../_models/Evento';
 })
 
 export class EventosComponent implements OnInit {
-  _filtroLista: string;
   
+  eventosFiltrados:  any = [];
+  eventos: any = [];
+  imagemLargura = 50;
+  imagemMargem = 2;
+  mostrarImagem = false;
+  modalRef: BsModalRef;
+  
+  _filtroLista= '';
+
+  constructor(
+    private eventoService: EventoService,
+    private modalService: BsModalService
+    ) { }
+
   get filtroLista(): string {
     return this._filtroLista;
   }
@@ -21,13 +35,11 @@ export class EventosComponent implements OnInit {
     this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
   }
 
-  eventosFiltrados:  any = [];
-  eventos: any = [];
-  imagemLargura = 50;
-  imagemMargem = 2;
-  mostrarImagem = false;
-  
-  constructor(private eventoService: EventoService) { }
+  openModal(template: TemplateRef<any>){
+
+    this.modalRef = this.modalService.show(template);
+  }
+
 
   ngOnInit() {
     this.getEventos();
