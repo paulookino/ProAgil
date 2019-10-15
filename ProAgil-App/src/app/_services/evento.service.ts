42964865ed63abdb42615b0d5025f331e6b46bcc
Okino Paulo <p.okino@reply.com>
2019-10-15 10:55:22 -0300

    71. Registrando Evento

--- ProAgil-App/src/app/_services/evento.service.ts
+++ ProAgil-App/src/app/_services/evento.service.ts
@@ -24,6 +24,10 @@ getEventoById(id: number): Observable<Evento[]>{
   return this.http.get<Evento[]>(`${this.baseURL}/${id}`);
 }
 
+postEvento(evento: Evento) {
+  return this.http.post(this.baseURL, evento);
+}
+
 }
 
 
--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -4,6 +4,7 @@ import { Evento } from '../_models/Evento';
 import {  BsModalService, BsModalRef } from 'ngx-bootstrap';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { defineLocale, BsLocaleService, ptBrLocale } from 'ngx-bootstrap';
+import { TemplateParseError } from '@angular/compiler';
 
 
 defineLocale('pt-br', ptBrLocale);
@@ -19,6 +20,7 @@ export class EventosComponent implements OnInit {
   
   eventosFiltrados:  any = [];
   eventos: any = [];
+  evento: Evento;
   imagemLargura = 50;
   imagemMargem = 2;
   mostrarImagem = false;
@@ -45,7 +47,7 @@ export class EventosComponent implements OnInit {
   }
 
   openModal(template: any){
-
+    this.registerForm.reset();
     template.show();
   }
 
@@ -81,8 +83,21 @@ validation(){
 
 }
 
-  salvarAlteracao(){
-
+  salvarAlteracao(template: any){
+      if(this.registerForm.valid)
+      {
+          this.evento = Object.assign({}, this.registerForm.value);
+          this.eventoService.postEvento(this.evento).subscribe(
+              (novoEvento: Evento) =>{
+                console.log(novoEvento);
+                template.hide();
+                this.getEventos();
+              }, error => {
+                console.log(error);
+              }
+              
+          );
+      }
 
   }
 
